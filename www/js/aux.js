7fb03b56aafc6dcb3892947f1c34677c75e483b4
Jasane Schio <saneschio@gmail.com>
2014-12-05 12:36:50 +0000

    asas

---
+++ www/js/aux.js
@@ -0,0 +1,9 @@
+BkpkApp.controller('Teste', function ($scope, $http, $state, $window, $log) {
+	var ids = [1,5,3,6,9];
+	$window.localStorage.removeItem('requests');
+//	$log.debug(ids);
+//$window.localStorage['requests'] =  JSON.stringify(ids);
+//	$log.info($window.localStorage['requests']);
+//	var newiD = JSON.parse($window.localStorage['requests']);
+//	$log.log(newiD);
+});
