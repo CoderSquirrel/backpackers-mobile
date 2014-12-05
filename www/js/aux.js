f48166f34b8d3e3899cf100237ddc0409b220c47
Jasane Schio <saneschio@gmail.com>
2014-12-05 12:59:24 +0000

    deletation

--- www/js/aux.js
+++
@@ -1,9 +0,0 @@
-BkpkApp.controller('Teste', function ($scope, $http, $state, $window, $log) {
-	var ids = [1,5,3,6,9];
-	$window.localStorage.removeItem('requests');
-//	$log.debug(ids);
-//$window.localStorage['requests'] =  JSON.stringify(ids);
-//	$log.info($window.localStorage['requests']);
-//	var newiD = JSON.parse($window.localStorage['requests']);
-//	$log.log(newiD);
-});
