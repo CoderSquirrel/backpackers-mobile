09465bc2d3aa0ea8e3ad1c61071af21c593c9409
Jasane Schio <saneschio@gmail.com>
2014-12-12 14:14:57 +0000

    build android apk

--- platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
+++ platforms/android/CordovaLib/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-/home/jasane/projects/BackpackersMobile/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
-/home/jasane/projects/BackpackersMobile/platforms/android/CordovaLib/AndroidManifest.xml \
+/home/jasane/projects/Projects/platforms/android/CordovaLib/ant-build/AndroidManifest.xml : \
+/home/jasane/projects/Projects/platforms/android/CordovaLib/AndroidManifest.xml \
--- platforms/android/ant-build/AndroidManifest.xml.d
+++ platforms/android/ant-build/AndroidManifest.xml.d
@@ -1,2 +1,2 @@
-/home/jasane/projects/BackpackersMobile/platforms/android/ant-build/AndroidManifest.xml : \
-/home/jasane/projects/BackpackersMobile/platforms/android/AndroidManifest.xml \
+/home/jasane/projects/Projects/platforms/android/ant-build/AndroidManifest.xml : \
+/home/jasane/projects/Projects/platforms/android/AndroidManifest.xml \
